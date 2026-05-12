f31a694b0cf7ced157b970a53a127e39036ef771
Gerard-Gon <gera.gonzalezs@duocuc.cl>
2026-05-12 00:02:40 -0400

    chore:Iniciando microservicio metricas innovatech

---
+++ src/test/java/ModuloMetricasInnovatech/Metricas/MetricasApplicationTests.java
@@ -0,0 +1,13 @@
+package ModuloMetricasInnovatech.Metricas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
